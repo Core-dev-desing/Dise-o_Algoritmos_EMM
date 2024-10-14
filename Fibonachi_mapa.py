d022fddfdae81954f64fc29fa4ed2be4c2052958
Core-dev-desing <13memeemm@gmail.com>
2024-10-13 22:37:01 -0600

    Add files via upload

---
+++ Fibonachi_mapa.py
@@ -0,0 +1,13 @@
+def Fibonachi(n):
+    dict{
+        0:1
+        1:1
+    }
+    n=2
+    if n<=1:
+        return 1
+    else:
+        Fibonachi(n)=Fibonachi(n-1)+Fibonachi(n-2)
+    print Fibonachi
+
+
---
+++ Fibonachirecu.py
@@ -0,0 +1,5 @@
+def Fibonachi(n):
+    if n<=1:
+        return 1
+    else:
+        Fibonachi(n)=Fibonachi(n-1)+Fibonachi(n-2)
