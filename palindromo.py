33d3ce35d04525426fa2f6926f98f6db1a0f3347
Core-dev-desing <13memeemm@gmail.com>
2024-10-18 04:20:21 -0600

    Add files via upload

---
+++ palindromo.py
@@ -0,0 +1,30 @@
+def Palíndromo(n)
+i=4
+n=10
+list A = {}
+
+while(k%n!=0){
+ if(i==4)
+     n=10000 
+     A[i]=k/n
+     k=k%n
+ else
+     if(i==3)
+       n=1000 
+       A[i]=k/n
+       k=k%n
+     else
+     	if(i==2)
+            n=100 
+            A[i]=k/n
+            k=k%n
+	else
+     	    IF(i==1)
+              n=10 
+              A[i]=k/n
+              k=k%n
+	    else
+               n=1 
+               A[i]=k/n
+               k=k%n
+}
