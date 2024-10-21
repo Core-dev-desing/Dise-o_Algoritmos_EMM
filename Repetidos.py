946a0841d32287ee82d0b7b4434c3306e8f586a1
Core-dev-desing <13memeemm@gmail.com>
2024-10-20 20:31:10 -0600

    Add files via upload

---
+++ Repetidos.py
@@ -0,0 +1,15 @@
+def repetidos(A)
+   
+    i=0
+    j=0
+    n_1=0
+
+    for (i=0; n-1)
+        n_1=i+3
+    
+        while (A[i+1]==A[i+n_1])
+            n_1=n_1+1
+        
+        A[i+1]=A[i+n_1]
+        j=j*1
+    return j    
